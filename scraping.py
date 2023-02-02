0e0349412abf2049f1e9335ef95fbbc535aa8f62
Nvmeless <59935640+Nvmeless@users.noreply.github.com>
2023-02-02 19:48:17 +0100

    correction by jinn

--- scraping.py
+++ scraping.py
@@ -4,14 +4,18 @@ from bs4 import BeautifulSoup
 
 # Url to scrap
 baseUrl = 'https://www.orpi.com'
-endUrl = '?transaction=rent&resultUrl=&realEstateTypes%5B0%5D=maison&realEstateTypes%5B1%5D=appartement&locations%5B0%5D%5Bvalue%5D=lyon-7&locations%5B0%5D%5Blabel%5D=Lyon%207%20%2869007%29&agency=&minSurface=&maxSurface=&minLotSurface=&maxLotSurface=&minStoryLocation=&maxStoryLocation=&newBuild=&oldBuild=&minPrice=&maxPrice=&sort=date-down&layoutType=mixte&page=&recentlySold=false'
+endUrl = '/recherche/rent?transaction=rent&resultUrl=&realEstateTypes%5B0%5D=maison&realEstateTypes%5B1%5D=appartement&agency=&minSurface=&maxSurface=&minLotSurface=&maxLotSurface=&minStoryLocation=&maxStoryLocation=&newBuild=&oldBuild=&minPrice=&maxPrice=&sort=date-down&layoutType=mixte&page=&recentlySold=false'
 
-# proxy
-response = requests.get (baseUrl + endUrl)
+# proxy 
+#Il ne faut pas mettre d'espace ici
+response = requests.get(baseUrl + endUrl)
 
 # Let's crawl on the website ! (take all the links)
 def crawl(theSoupy): 
+    #il n'y a rien dans la div que tu vise (apres j'ai testé avec mon lien hein.. )
+    #Mais au moins tu rentre dans ta fonction sans erreurs
     div = theSoupy.find('div', {"class": "c-box__inner c-box__inner--sm c-overlay"})
+    print(div)
     spans = div.findAll('span')
     for span in spans:
         a = span.find('a')
@@ -24,3 +28,6 @@ def crawl(theSoupy):
 # parser
 if response.ok:
     soupySoup = BeautifulSoup(response.text,'html.parser')
+
+    #Tu as oublié d'appeler la fonction crawl :
+    crawl(soupySoup)
